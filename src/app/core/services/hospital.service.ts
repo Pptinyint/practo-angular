2bc03c26e5ca9e512de495e4c8bd863bc00bb070
Pptinyint <pratiksha.mail@gmail.com>
2026-04-24 13:17:31 +0530

    adding more code

--- src/app/core/services/hospital.service.ts
+++ src/app/core/services/hospital.service.ts
@@ -28,24 +28,11 @@
       return this.http.post<IApiResponse<IUser>>(environment.api_url+Constant.API_END_POINT.USER_LOGIN,loginObj)
     }
 
-    // /getAppointmentsByHospitalId(){
-    
-  // getAppointmentsByHospital(loginObj:IAppointment): Observable<IApiResponse<IAppointment[]>>{
-  //     return this.http.get<IApiResponse<IAppointment[]>>(environment.api_url+Constant.API_END_POINT.GET_APPOINTMENTS_BY_HOSTPITAL_ID+loginObj.hospitalId)
-    
-  // }
   createAppointment(obj:IAppointment): Observable<IApiResponse<IAppointment>>{
       return this.http.post<IApiResponse<IAppointment>>(environment.api_url+Constant.API_END_POINT.CREATE_NEW_APPOINTMENT,obj)
     }
 
 
-
-
-  // getAppointmentsByHospitalId(id: number): Observable<IApiResponse<IAppointment[]>> {
-  //   return this.http.get<IApiResponse<IAppointment[]>>(
-  //     environment.api_url + Constant.API_END_POINT.GET_APPOINTMENTS_BY_HOSTPITAL_ID + "/" + id
-  //   );
-  // }
 
 getAppointmentsByHospitalId(id: number, userName: string): Observable<IApiResponse<IAppointment[]>> {
   return this.http.get<IApiResponse<IAppointment[]>>(
